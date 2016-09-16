cad085731de81aa5ae8c4d44e26eddae1f85e81b
zhq3211 <812842168@qq.com>
2016-09-16 15:12:02 +0800

    Modify: BaseCode.c

--- BaseCode/BaseCode.c
+++ BaseCode/BaseCode.c
@@ -3,7 +3,7 @@
 //************************
 
 #include <Uefi.h>
-#include <Library\UefiLib.h>  //Print()  
+#include <Library\UefiLib.h>  //Print()
 
 EFI_STATUS
 EFIAPI
@@ -12,11 +12,20 @@ UefiMain (
   IN EFI_SYSTEM_TABLE	*SystemTable
   )
 {
+  //Print
   //Use UefiLib Print API to print string to UEFI console
   Print(L"Hi,everyone. This is the most basic UEFI APP!\n");
   Print(L"Are OK?\n");
-  //SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello\n");
-  //SystemTable->ConOut->OutputString(SystemTable->ConOut, L"world!\n");
- 
+
+  //ConOut
+  //顶格输出
+  //换行后，下一行依旧是紧跟前一行的字符追加！
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\n");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello\n");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"world!\n");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Are");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"you");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"OK?");
+
   return EFI_SUCCESS;
 }
